cd65319837e97811a3b059e68d42ff32564381fe
JUSTIN ARRUDA <justinontheinternet@users.noreply.github.com>
2016-07-19 11:36:07 -0400

    implemented options page and reset

---
+++ options.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  var goal = document.getElementById('goal');
+  var saveButton = document.getElementById('save');
+  var resetButton = document.getElementById('reset');
+
+  chrome.storage.sync.get('goal', function(items) {
+    if (items.goal) {
+      goal.value = items.goal;
+    }
+  });
+
+  save.addEventListener('click', function() {
+
+    var goalVal = goal.value;
+    if (goalVal) {
+      chrome.storage.sync.set({ 'goal': goalVal }, function() {
+        close();
+      });
+    }
+  });
+
+  reset.addEventListener('click', function() {
+    chrome.storage.sync.set({ 'total': 0 });
+  });
+
+});
